3745bdcdad6566f55a539bf161bb0a719927c5d1
Jack Carter <jack.carter334@gmail.com>
2023-02-24 13:59:44 +0000

    Add separate file for testing freq. analysis. Plot chart displaying word count to accuracy

--- Algorithm_Development/Frequency Analysis/main.py
+++ Algorithm_Development/Frequency Analysis/main.py
@@ -3,61 +3,51 @@ import re, sys, os
 current = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(current)
 sys.path.append(parent_directory)
-
 from Algorithm_Development.accuracy_test import test_accuracy
-letter_frequency_in_text = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0,
-                            "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0,
-                            "w": 0, "x": 0, "y": 0, "z": 0}
-
-letter_frequency_mapping = {"a": "", "b": "", "c": "", "d": "", "e": "", "f": "", "g": "", "h": "", "i": "", "j": "",
-                            "k": "", "l": "", "m": "", "n": "", "o": "", "p": "", "q": "", "r": "", "s": "", "t": "",
-                            "u": "", "v": "", "w": "", "x": "", "y": "", "z": ""}
-
-actual_mapping = {"a": "k", "b": "x", "c": "v", "d": "m", "e": "c", "f": "y", "g": "o", "h": "p", "i": "h", "j": "q",
-                            "k": "r", "l": "s", "m": "z", "n": "f", "o": "i", "p": "j", "q": "a", "r": "d", "s": "l", "t": "e",
-                            "u": "g", "v": "w", "w": "b", "x": "u", "y": "n", "z": "t"}
-
-english_letter_frequencies = ["e", "t", "a", "o", "i", "n", "s", "r", "h", "d", "l",  "u", "c", "m", "f",
-                              "y", "w",  "g", "p", "b", "v", "k", "x", "q", "j", "z"]
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-key_string = ""
-
 # Extract words
-f = open("../Test_Texts/Encrypted/encrypted_long_story.txt", "r", encoding="utf-8")
-words = " ".join(re.findall("[a-zA-Z]+", f.read())).lower()
+# f = open(file, "r", encoding="utf-8")
+#     words = " ".join(re.findall("[a-zA-Z]+", f.read())).lower()
+def perform_frequency_analysis(words):
+    letter_frequency_in_text = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0,
+                                "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0,
+                                "w": 0, "x": 0, "y": 0, "z": 0}
 
+    letter_frequency_mapping = {"a": "", "b": "", "c": "", "d": "", "e": "", "f": "", "g": "", "h": "", "i": "",
+                                "j": "",
+                                "k": "", "l": "", "m": "", "n": "", "o": "", "p": "", "q": "", "r": "", "s": "",
+                                "t": "",
+                                "u": "", "v": "", "w": "", "x": "", "y": "", "z": ""}
+
+    english_letter_frequencies = ["e", "t", "a", "o", "i", "n", "s", "r", "h", "d", "l", "u", "c", "m", "f",
+                                  "y", "w", "g", "p", "b", "v", "k", "x", "q", "j", "z"]
 
 # Find frequency of letters
-for key, value in letter_frequency_in_text.items():
-    for letter in words:
-        if letter == key:
-            letter_frequency_in_text[key] += 1
+    for key, value in letter_frequency_in_text.items():
+        for letter in words:
+            if letter == key:
+                letter_frequency_in_text[key] += 1
 
-letter_frequency_in_text = sorted(letter_frequency_in_text.items(), key=lambda x: x[1], reverse=True)
-print("Letter Frequency: ", letter_frequency_in_text)
+    letter_frequency_in_text = sorted(letter_frequency_in_text.items(), key=lambda x: x[1], reverse=True)
+
+    # Map English letter frequencies to input letter frequencies
+    for i in range(len(letter_frequency_in_text)):
+        letter_frequency_mapping[(letter_frequency_in_text[i][0])] = english_letter_frequencies[i]
+
+    return letter_frequency_mapping
 
 
-# Map English letter frequencies to input letter frequencies
-for i in range(len(letter_frequency_in_text)):
-    print(
-        f"Letter: {letter_frequency_in_text[i]} being set to: {english_letter_frequencies[i]}")
-    letter_frequency_mapping[(letter_frequency_in_text[i][0])] = english_letter_frequencies[i]
-
-print(f"Complete Mapping: {letter_frequency_mapping}")
-
+letter_frequency_mapping = perform_frequency_analysis("../Test_Texts/Encrypted/encrypted_long_story.txt")
+key_string = ""
 
 for key, value in letter_frequency_mapping.items():
     key_string += value
 
-print(key_string)
-print(alphabet)
-mapping = str.maketrans(alphabet, key_string)
-words = words.translate(mapping)
+# mapping = str.maketrans(alphabet, key_string)
+# words = words.translate(mapping)
 
-print(words)
-print(f"Accuracy: {test_accuracy(letter_frequency_mapping)}")
-print(actual_mapping)
-print(letter_frequency_mapping)
+# print(words)
+# print(f"Accuracy: {test_accuracy(letter_frequency_mapping)}")
 
---
+++ Algorithm_Development/Frequency Analysis/test_frequency_analysis.py
@@ -0,0 +1,53 @@
+import re, sys, os
+from main import perform_frequency_analysis
+import numpy as np
+from matplotlib import pyplot as plt
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current)
+sys.path.append(parent_directory)
+from Algorithm_Development.accuracy_test import test_accuracy
+import matplotlib as mpl
+mpl.use('TkAgg')
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+word_count = []
+accuracy_list = []
+
+# Get encrypted test files
+test_files = []
+for root, dirs, files in os.walk("../Test_Texts/Encrypted"):
+    print(f"Root: {root}, Dirs: {dirs}, Files: {files}")
+    for file in files:
+        test_files.append(root + "/" + file)
+
+# Perform frequency analysis
+for file in test_files:
+    key_string = ""
+    f = open(file, "r", encoding="utf-8")
+    words = " ".join(re.findall("[a-zA-Z]+", f.read())).lower()
+    word_count.append(len(words.split(" ")))
+    letter_frequency_mapping = perform_frequency_analysis(words)
+    accuracy_list.append(test_accuracy(letter_frequency_mapping))
+
+    for key, value in letter_frequency_mapping.items():
+        key_string += value
+    mapping = str.maketrans(alphabet, key_string)
+    words = words.translate(mapping)
+    print(words)
+
+
+# Sort the word counts and accuracies relative to each other using word count
+sort = np.argsort(np.array(word_count))
+word_count = np.array(word_count)[sort]
+accuracy_list = np.array(accuracy_list)[sort]
+
+for i in range(len(accuracy_list)):
+    print(f"Accuracy for word count {word_count[i]} is {accuracy_list[i]}/26")
+
+plt.title("Frequency Analysis")
+plt.xlabel("Word Count")
+plt.ylabel("Accuracy")
+plt.plot(word_count, accuracy_list)
+plt.show()
