3874c64240112ede7b10b3221a2dd10875bdd890
Jack Carter <jack.carter334@gmail.com>
2023-02-24 12:34:14 +0000

    Add option to accuracy test to turn off output

--- Algorithm_Development/accuracy_test.py
+++ Algorithm_Development/accuracy_test.py
@@ -2,19 +2,21 @@ actual_mapping = {"a": "k", "b": "x", "c": "v", "d": "m", "e": "c", "f": "y", "g
                             "k": "r", "l": "s", "m": "z", "n": "f", "o": "i", "p": "j", "q": "a", "r": "d", "s": "l", "t": "e",
                             "u": "g", "v": "w", "w": "b", "x": "u", "y": "n", "z": "t"}
 
-def test_accuracy_with_custom_mapping(calculated_mapping, actual_mapping):
+def test_accuracy_with_custom_mapping(calculated_mapping, actual_mapping, show_output=False):
     total_correct = 0
     for key, value in calculated_mapping.items():
-        print(f"Calculated Mapping Letter: {calculated_mapping[value]}. Actual Mapping Letter: {actual_mapping[value]}")
+        if show_output:
+            print(f"Calculated Mapping Letter: {calculated_mapping[value]}. Actual Mapping Letter: {actual_mapping[value]}")
         if calculated_mapping[value] == actual_mapping[value]:
             total_correct += 1
     return total_correct
 
 
-def test_accuracy(calculated_mapping):
+def test_accuracy(calculated_mapping, show_output=False):
     total_correct = 0
     for key, value in calculated_mapping.items():
-        print(f"Calculated Mapping Letter: {calculated_mapping[value]}. Actual Mapping Letter: {actual_mapping[value]}")
+        if show_output:
+            print(f"Calculated Mapping Letter: {calculated_mapping[value]}. Actual Mapping Letter: {actual_mapping[value]}")
         if calculated_mapping[value] == actual_mapping[value]:
             total_correct += 1
     return total_correct
