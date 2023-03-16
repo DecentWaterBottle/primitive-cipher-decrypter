f92b71b0145b1ea22c263a7056617107e76cd242
Jack Carter <jack.carter334@gmail.com>
2023-03-16 14:08:33 +0000

    Add encrypted version of each text

--- Algorithm_Development/text_encrypter.py
+++ Algorithm_Development/text_encrypter.py
@@ -1,6 +1,15 @@
 
-text_to_encrypt = """"""
-key = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+text_to_encrypt = input("Text: ")
+# Key for characters text_1
+# key = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+# Key for characters text_2
+# key = "KXVMCNOPHQRSZYIJADLEGWBUFTkxvmcnophqrszyijadlegwbuft"
+# Key for characters text_3
+# key = "EBFASWLYGQTZOHDKUCPRNXIVMJebfaswlygqtzohdkucprnxivmj"
+# Key for characters text_4
+# key = "HMFTKQDERYZJBGPUAWOSNXICVLhmftkqderyzjbgpuawosnxicvl"
+# Key for characters text_5
+key = "CTLSXAHGBPREQJUMKOWVIFDYZNctlsxahgbpreqjumkowvifdyzn"
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 mapping = str.maketrans(alphabet, key)
