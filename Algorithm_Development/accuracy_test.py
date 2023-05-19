7bbe56d88fce9b470b6c965d4887e741bf29f257
Jack <jack.carter334@gmail.com>
2023-05-19 19:15:43 +0100

    Add helper functions to test key accuracy

--- Algorithm_Development/accuracy_test.py
+++ Algorithm_Development/accuracy_test.py
@@ -1,12 +1,18 @@
-actual_mapping = {"a": "k", "b": "x", "c": "v", "d": "m", "e": "c", "f": "y", "g": "o", "h": "p", "i": "h", "j": "q",
-                            "k": "r", "l": "s", "m": "z", "n": "f", "o": "i", "p": "j", "q": "a", "r": "d", "s": "l", "t": "e",
-                            "u": "g", "v": "w", "w": "b", "x": "u", "y": "n", "z": "t"}
+
+actual_mapping = {"a": "q", "b": "w", "c": "e", "d": "r", "e": "t", "f": "y", "g": "u", "h": "i", "i": "o",
+                      "j": "p",
+                      "k": "a", "l": "s", "m": "d", "n": "f", "o": "g", "p": "h", "q": "j", "r": "k", "s": "l",
+                      "t": "z",
+                      "u": "x", "v": "c", "w": "v", "x": "b", "y": "n", "z": "m"}
+actual_mapping = {v: k for k, v in actual_mapping.items()}
+
 
 def test_accuracy_with_custom_mapping(calculated_mapping, actual_mapping, show_output=False):
     total_correct = 0
     for key, value in calculated_mapping.items():
         if show_output:
-            print(f"Calculated Mapping Letter: {calculated_mapping[value]}. Actual Mapping Letter: {actual_mapping[value]}")
+            print(f"Calculated Mapping Letter: {calculated_mapping[value]}. "
+                  f"Actual Mapping Letter: {actual_mapping[value]}")
         if calculated_mapping[value] == actual_mapping[value]:
             total_correct += 1
     return total_correct
